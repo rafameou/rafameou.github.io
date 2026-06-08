dc0f3a40bff52f40b81f90239f15ae2c01bbd70e
Rafael Mazzutti <rafaelmazz22@gmail.com>
2026-06-08 14:39:44 -0300

    typer 0.6.0

--- typer/index.service.worker.js
+++ typer/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1780934551|109644318';
+const CACHE_VERSION = '1780940376|5934268706';
 /** @type {string} */
 const CACHE_PREFIX = 'typer-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
